7684ebaa375d6010ce88d2804149af6656275b06
Aye Thuzar <ayethuzar@gmail.com>
2023-07-22 18:50:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ data_df = {
 
 data_df = pd.DataFrame.from_dict(data_df)
 
-data_df_normal = scaler.transform(data_df)
+#data_df_normal = scaler.transform(data_df)
 
 y_pred_base = lgbm_base.predict(data_df)
 y_pred_optimized = lgbm_opt.predict(data_df)
