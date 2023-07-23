56a413aee5715c20a54b2b5430afdbf86070a8f2
Aye Thuzar <ayethuzar@gmail.com>
2023-07-23 09:29:01 -0400

    app.py update

--- app.py
+++ app.py
@@ -1,41 +1,25 @@
 
-
+# importing libraries
 import streamlit as st
 import pandas as pd
 import numpy as np
-import tensorflow as tf
+import pickle
+import shap
 
-from sklearn.preprocessing import MinMaxScaler
-
-import tensorflow_probability as tfp
-tfd = tfp.distributions
-
-from pickle import dump
-from pickle import load
-
-
-
-scaler = MinMaxScaler()
-
-
-# load trained model
-lgbm_base = pickle.load(open('lgbm_base.pkl', 'rb'))
-lgbm_opt = pickle.load(open('lgbm_optimized.pkl', 'rb'))
-
-
-tf.random.set_seed(42)
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 np.random.seed(42)
 
-
 st.markdown("<body style ='color:#E2E0D9;'></body>", unsafe_allow_html=True)
 
+st.markdown("<h1 style='text-align: center; color: #1B9E91;'>House Price Prediction in Ames, Iowa</h1>", unsafe_allow_html=True)
+
+st.write("If you want to know the numbers that you picked for some of the features such as Overall Quality, Sale Conditions etc., please check the following link")
+st.write("[link to the categorical encoding](https://github.com/aye-thuzar/CS634Project/edit/main/docs.md)")
 
 
-st.markdown("<h4 style='text-align: center; color: #1B9E91;'>House Price Prediction in Ames,Iowa</h4>", unsafe_allow_html=True)
 
-st.markdown("<h5 style='text-align: center; color: #1B9E91;'>A multi-step process is used to estimate the range of house prices based on your selection. </h5>", unsafe_allow_html=True)
-
+#setting up the sliders and getting the input from the sliders
 
 name_list = [
  'OverallQual',
@@ -49,48 +33,30 @@ name_list = [
  'SaleCondition',
  'BsmtExposure']
 
-name_list_train = [
- 'OverallQual',
- 'YearBuilt',
- 'TotalBsmtSF',
- 'GrLivArea',
- 'MasVnrArea',
- 'BsmtFinType1',
- 'Neighborhood',
- 'GarageType',
- 'SaleCondition',
- 'BsmtExposure']
-
-data = pd.read_csv('train.csv')
-
-
-data = data[name_list_train].values
-
-scaler.fit(data)
-
 description_list = [
  'What is the Overall material and finish quality?',
  'In which year was the Original construction date?',
  'What is the Total square feet of basement area?',
  'What is the Above grade (ground) living area in square feet?',
- 'What is the Masonry veneer area in square feet??',
- 'What is the Quality of basement finished area?',
- 'Where is the physical locations within Ames city limits?',
+ 'What is the Masonry veneer area in square feet?',
+ 'What is the Quality of the basement finished area?',
+ 'Where are the physical locations within Ames city limits?',
  'Where is the location of the Garage?',
  'What is the condition of the sale?',
- 'Does the house have walkout or garden-level basement walls?'
+ 'What is the basement exposure: walkout or garden-level basement walls?'
  ]
 
-min_list = [1.0,
+min_list = [
+ 1.0,
  1950.0,
- 0.0,
- 0.0,
+ 800.0,
+ 500.0,
  334.0,
- 0.0,
- 0.0,
- 0.0,
- 0.0,
- 0.0
+ 1.0,
+ 1.0,
+ 1.0,
+ 1.0,
+ 1.0
 ]
 
 max_list = [
@@ -99,11 +65,11 @@ max_list = [
  2336.0,
  6110.0,
  4692.0,
- 10.0,
- 10.0,
- 3.0,
- 10.0,
- 1.0,
+ 7.0,
+ 25.0,
+ 7.0,
+ 6.0,
+ 5.0,
 ]
 
 count = 0
@@ -119,8 +85,6 @@ with st.sidebar:
       
     st.write("[Kaggle Link to Data Set](https://www.kaggle.com/competitions/house-prices-advanced-regression-techniques)")
 
-    
-
 
 data_df = {
 
@@ -128,24 +92,51 @@ data_df = {
  'YearBuilt': [YearBuilt],
  'TotalBsmtSF': [TotalBsmtSF],
  'GrLivArea':[GrLivArea],
- 'MasVnrArea': [MasVnrArea]
- 'BsmtFinType1': [BsmtFinType1]
- 'Neighborhood': [Neighborhood]
- 'GarageType': [GarageType]
- 'SaleCondition': [SaleCondition]
+ 'MasVnrArea': [MasVnrArea],
+ 'BsmtFinType1': [BsmtFinType1],
+ 'Neighborhood': [Neighborhood],
+ 'GarageType': [GarageType],
+ 'SaleCondition': [SaleCondition],
  'BsmtExposure': [BsmtExposure]
 }
 
-#negloglik = lambda y, p_y: -p_y.log_prob(y) # note this
-
 data_df = pd.DataFrame.from_dict(data_df)
 
-data_df_normal = scaler.transform(data_df)
-
-y_pred_base = lgbm_base.predict(data_df)
-y_pred_optimized = lgbm_opt.predict(data_df)
+st.write("Please adjust the feature values using the slides on the left: ")
+st.write(data_df.head())
 
 
+#normalizing the data
+
+diff = np.array(max_list)-np.array(min_list)
+data_df_normalized = (data_df.values - np.array(min_list)) / diff
+
+#st.write("Normalized input data")
+#st.write(data_df)
+
+# load trained models
+lgbm_base = pickle.load(open('lgbm_base.pkl', 'rb'))
+lgbm_opt = pickle.load(open('lgbm_optimized.pkl', 'rb'))
+xgb_base = pickle.load(open('xgb_base.pkl', 'rb'))
+xgb_opt = pickle.load(open('xgb_optimized.pkl', 'rb'))
+
+
+
+y_pred = xgb_base.predict(data_df_normalized)
+y_pred_optimized = xgb_opt.predict(data_df_normalized)
+
+# SHAP for the base model
+explainer_base = shap.TreeExplainer(xgb_base)
+shap_interaction_base = explainer_base.shap_interaction_values(data_df)
+# Get SHAP values
+shap_values_base = explainer_base(data_df)
+
+
+# SHAP for the optimized model
+explainer_opt = shap.TreeExplainer(xgb_opt)
+shap_interaction_opt = explainer_opt.shap_interaction_values(data_df)
+# Get SHAP values
+shap_values_opt = explainer_opt(data_df)
 
 col1, col2, col3 , col4, col5 = st.columns(5)
 
@@ -171,47 +162,45 @@ if center_button:
     with st.spinner('Calculating....'):
         time.sleep(2)
 
+    base_model_prediction = "{:,.2f}".format(y_pred[0])
+    optimized_model_prediction = "{:,.2f}".format(y_pred_optimized[0])
 
+    st.write("")
 
-    st.markdown("<h5 style='text-align: center; color: #1B9E91;'>The price range of your house is between:</h5>", unsafe_allow_html=True)
+    result1 = "Base model's prediciton: $" + str(base_model_prediction)
+    html_str = f"""<style>p.a {{font: bold {28}px Courier;color:#1D5D9B;}}</style><p class="a">{result1}</p>"""   
+    st.markdown(html_str, unsafe_allow_html=True)
 
+    st.markdown("<h4 style='text-align: center; color: #1B9E91;'>SHAP Summary Plot</h4>", unsafe_allow_html=True)
+    st.markdown("<div style='text-align: center;'>", unsafe_allow_html=True)
+    shap.plots.beeswarm(shap_values_base, max_display=10)
+    st.markdown("</div>", unsafe_allow_html=True)
+    st.pyplot()
 
-    col1, col2 = st.columns([3, 3])
+    st.markdown("<h4 style='text-align: center; color: #1B9E91;'>SHAP Interaction Plot</h4>", unsafe_allow_html=True)
+    st.markdown("<div style='text-align: center;'>", unsafe_allow_html=True)
+    shap.summary_plot(shap_interaction_base, max_display=10)
+    st.markdown("</div>", unsafe_allow_html=True)
+    st.pyplot()
+ 
+    result2 = "Optimized model's prediciton: $" + str(optimized_model_prediction)
+    html_str2 = f"""<style>p.a {{font: bold {28}px Courier;color:#1D5D9B;}}</style><p class="a">{result2}</p>"""
+    st.markdown(html_str2, unsafe_allow_html=True)
 
-    lower_number = "{:,.2f}".format(int(yhat.mean().numpy()-1.95*yhat.stddev().numpy()))
-    higher_number = "{:,.2f}".format(int(yhat.mean().numpy()+1.95*yhat.stddev().numpy()))
+    st.markdown("<h4 style='text-align: center; color: #1B9E91;'>SHAP Summary Plot</h4>", unsafe_allow_html=True)
+    st.markdown("<div style='text-align: center;'>", unsafe_allow_html=True)
+    shap.plots.beeswarm(shap_values_opt, max_display=10)
+    st.markdown("</div>", unsafe_allow_html=True)
+    st.pyplot()
 
-    col1, col2, col3 = st.columns(3)
+    st.markdown("<h4 style='text-align: center; color: #1B9E91;'>SHAP Interaction Plot</h4>", unsafe_allow_html=True)
+    st.markdown("<div style='text-align: center;'>", unsafe_allow_html=True)
+    shap.summary_plot(shap_interaction_opt, max_display=10)
+    st.markdown("</div>", unsafe_allow_html=True)
+    st.pyplot()
+    
 
     
 
-    with col1:
-        st.write("")
-
-    with col2:
-        st.subheader("USD "+ str(lower_number))
-        st.subheader("       AND ")
-
-        st.subheader(" USD "+str(higher_number))
-
-
-    with col3:
-        st.write("")
-
-    
-
-    
-
-    import base64
-
-    file_ = open("kramer_gif.gif", "rb")
-    contents = file_.read()
-    data_url = base64.b64encode(contents).decode("utf-8")
-    file_.close()
-
-    st.markdown(
-        f'<center><img src="data:image/gif;base64,{data_url}" alt="cat gif"></center>',
-        unsafe_allow_html=True,
-    )
-    
+  
 
